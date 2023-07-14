bda633de1a5dfd7d48a03e6a402b1f61d7ace227
BaDong <dongba352001@gmail.com>
2023-07-14 13:46:36 +0700

    push

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>HACK</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to
         </p>
-      </div>
+      </div> */}
       <p className="read-the-docs">HACK</p>
     </>
   );
