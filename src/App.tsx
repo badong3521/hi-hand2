c081f51a28dd16a1cd3e76015b00a3a0728f3b2a
BaDong <dongba352001@gmail.com>
2023-07-15 23:05:43 +0700

    done setup

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to
         </p>
       </div> */}
-      <p className="read-the-docs">2</p>
+      <p className="read-the-docs">3</p>
     </>
   );
 }
